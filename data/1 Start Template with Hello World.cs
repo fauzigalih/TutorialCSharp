c68b6667809827afe2480907187c548cc46dda72
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-05-28 19:45:54 +0700

    Initial commit

---
+++ data/1 Start Template with Hello World.cs
@@ -0,0 +1,13 @@
+using System; //library
+
+namespace TutorialCSharp  //namespace
+{
+    class Program //class
+    {
+        static void Main(string[] args) //method special, automatic run program on this Main method and call a members
+        {
+            Console.WriteLine("Hello World!"); //members in method
+        }
+    }
+
+}
