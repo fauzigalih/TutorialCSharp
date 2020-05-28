6ddb8269631d9c4a15f09d7abc836c3a1ad772a8
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-05-28 20:18:52 +0700

    Create 2 Method, Static Method, Instance Method.cs

---
+++ data/2 Method, Static Method, Instance Method.cs
@@ -0,0 +1,28 @@
+using System; //library
+
+namespace TutorialCSharp //namespace
+{
+    class Program //class
+    {
+        static void Main(string[] args) //method special, automatic run program on this Main method and call a statement
+        {
+            Console.WriteLine("Hello World!"); //a statement
+
+            IsStatic(); //can call this method without initialization of class Program, this is same with (Program.IsStatic();)
+
+            Program myProgram = new Program(); //initialization class of Program -> step 1
+            myProgram.NotStatic(); //call method with initialization of class Program -> step 2
+        }
+
+        public void NotStatic() //only method
+        {
+            Console.WriteLine("This is only method!"); //statement
+        }
+
+        public static void IsStatic() //static method
+        {
+            Console.WriteLine("This is static method!"); //statement
+        }
+    }
+
+}
