4458418e7529db53d8d89a9e66da0a018d83e916
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-05-28 21:40:05 +0700

    Assignment Value Type and Parameter

--- data/6 Assignment Value Type and Parameter.cs
+++ data/6 Assignment Value Type and Parameter.cs
@@ -7,12 +7,12 @@ namespace TutorialCSharp //namespace
         static void Main(string[] args) //method special, automatic run program on this Main method and call a statement
         {
             int a = 5; //declaration and initialization variable 
-            Print(a); //call a method with value a
+            Print(a); //this call a method with value a
         }
 
         public static void Print(int a) //static method with parameter int a -> Print(5) -> a = 5
         {
-            int x = a + 5; //declaration and initialization variable x with sum parameter and 5
+            int x = a + 5; //declaration and initialization variable x with sum parameter + 5
             Console.WriteLine(x); //print statement variable x
         }
     }
