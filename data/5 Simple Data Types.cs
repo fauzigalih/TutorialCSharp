794a8ba27675824a029f47d4036807ec8f2431b8
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-05-28 21:16:31 +0700

    Simple Data Types

---
+++ data/5 Simple Data Types.cs
@@ -0,0 +1,22 @@
+using System; //library
+
+namespace TutorialCSharp //namespace
+{
+    class Program //class
+    {
+        static void Main(string[] args) //method special, automatic run program on this Main method and call a statement
+        {
+            int a = -5; //int can positive and negative
+            uint b = 5; //uint only can positive
+            char c = 'C'; //char is one character or unicode with single quote (ASCII Code)
+            string d = "Hello"; //string is many character with double quote
+            double e = 5.5; //double is decimal number
+            float f = 5.5f; //float is decimal number with use low memory than double (syntax f after the number)
+            decimal g = 5.5m; //decimal is decimal number with high precision, usually in money (syntax m after the number)
+            bool h = true; //bool is 2 conditional true or false
+
+            //you can see full information types data c sharp in url : https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/types-and-variables
+        }
+    }
+
+}
