89d04ddd895f7e945873ea22ed23baf0a6df1002
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-05-28 20:32:09 +0700

    User Input Dynamic Program

---
+++ data/3 User Input Dynamic Program.cs
@@ -0,0 +1,15 @@
+using System; //library
+
+namespace TutorialCSharp //namespace
+{
+    class Program //class
+    {
+        static void Main(string[] args) //method special, automatic run program on this Main method and call a statement
+        {
+            Console.Write("Enter your name : "); //statement without new line
+            string name = Console.ReadLine(); //initialization variable name and assign with input type
+            Console.WriteLine("Your name is " + name); //statement concat with variable name (input type)
+        }
+    }
+
+}
