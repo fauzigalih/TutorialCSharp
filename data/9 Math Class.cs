3935614b7cd6c7a2ac319545f61fbd4afa03cb2c
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-05-29 21:47:21 +0700

    Math Class

---
+++ data/9 Math Class.cs
@@ -0,0 +1,22 @@
+using System; //library
+
+namespace TutorialCSharp //namespace
+{
+    class Program //class
+    {
+        static void Main(string[] args) //method special, automatic run program on this Main method and call a statement
+        {
+            double x = -5.5; //variable
+            double y = 64; //variable
+
+            Math.Max(x, y); //method can be used to find the highest value of x and y
+            Math.Min(x, y); //method can be used to find the lowest value of of x and y
+            Math.Sqrt(y); //method returns the square root of x
+            Math.Abs(x); //method returns the absolute (positive) value of x
+            Math.Round(x); //rounds a number to the nearest whole number
+
+            //You can check other Math class in link : https://docs.microsoft.com/en-us/dotnet/api/system.math?view=netcore-3.1
+        }
+    }
+
+}
