4d5cb7540429d57a640cb8fb9ef76a081d191743
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-05-29 21:22:59 +0700

    Implicit and Explicit Conversion

---
+++ data/7 Implicit and Explicit Conversion.cs
@@ -0,0 +1,21 @@
+using System; //library
+
+namespace TutorialCSharp //namespace
+{
+    class Program //class
+    {
+        static void Main(string[] args) //method special, automatic run program on this Main method and call a statement
+        {
+            //Implicit Conversion -> description below
+            // if the data type to be converted has a smaller size or range of values ​​than the result of the converted data type.
+            int a = 5; //32 bit integer
+            long b = a; //64 bit integer
+
+            //Explicit Conversion -> description below
+            //If the two conditions where conversion is implicitly possible are not met, then we must use conversion explicitly. This explicit conversion is usually called casting.
+            double c = 5.5; //8 byte
+            int d = (int)c; //4 byte
+        }
+    }
+
+}
