baf3a4ba2f6dde54b31518ed63a7923b3f21b327
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-05-30 21:44:11 +0700

    While Loop

---
+++ data/19 While Loop.cs
@@ -0,0 +1,18 @@
+using System; //library
+
+namespace TutorialCSharp //namespace
+{
+    class Program //class
+    {
+        static void Main(string[] args) //method special, automatic run program on this Main method and call a statement
+        {
+            //While Loop
+            int i = 1; //variable integer
+            while(i <= 10) //condition
+            {
+                Console.WriteLine(i); //print of variable in loop
+                i++; //increment
+            }
+        }
+    }
+}
