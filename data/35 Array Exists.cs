f6ae6796bee5b264e9d16bb6911078b79b7ae51c
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-06-01 22:17:32 +0700

    Array Exists

--- data/35 Array Exists.cs
+++ data/35 Array Exists.cs
@@ -7,8 +7,8 @@ namespace TutorialCSharp //namespace
         static void Main(string[] args) //method special, automatic run program on this Main method and call a statement
         {
             string[] names = { "Fauzi", "Galih" }; //array
-            bool exist = Array.Exists(names, element => element == "Fauzi"); //method array exist -> true or false
-            if (exist) Console.WriteLine("Its my first name.."); //condition array exist
+            bool exists = Array.Exists(names, element => element == "Fauzi"); //method array exist -> true or false
+            if (exists) Console.WriteLine("Its my first name.."); //condition array exists
             else Console.WriteLine("Not my name..");
         }
     }
