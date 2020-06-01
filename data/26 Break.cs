85c7fcf7f16f69a68abaa095dd0445c0e4d89891
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-06-01 12:07:34 +0700

    Break

---
+++ data/26 Break.cs
@@ -0,0 +1,21 @@
+using System; //library
+
+namespace TutorialCSharp //namespace
+{
+    class Program //class
+    {
+        static int x = 5; //variable int global
+        static void Main(string[] args) //method special, automatic run program on this Main method and call a statement
+        {
+            for(int i = 9; i > 0; i--) //for loop
+            {
+                for(int j = i; j > 0; j--) //for loop
+                {
+                    if (j == 3) break; //break is stoped for loop
+                    Console.Write(j + " "); //printout
+                }
+                Console.WriteLine(); //new line
+            }
+        }
+    }
+}
