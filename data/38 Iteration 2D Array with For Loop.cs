9bc7abd4fa929db0c95b60a8b984f27fa962217a
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-06-01 23:03:19 +0700

    Iteration 2D Array with For Loop

---
+++ data/38 Iteration 2D Array with For Loop.cs
@@ -0,0 +1,29 @@
+using System; //library
+
+namespace TutorialCSharp //namespace
+{
+    class Program //class
+    {
+        static void Main(string[] args) //method special, automatic run program on this Main method and call a statement
+        {
+            //2D Array
+            int[,] numbers = 
+            {
+                { 1, 2, 3, 4 },
+                { 2, 4, 6, 8 },
+            };
+            Console.WriteLine("You have array of numbers:");
+            for(int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for(int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    Console.Write(numbers[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //numbers.Length; output: 8 (total all array)
+            //numbers.GetLength(0); output: 1 2 3 4 (total row index 0 of array)
+        }
+    }
+}
