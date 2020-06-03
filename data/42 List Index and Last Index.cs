6ed7248dfe2984bcaeaafc2036aaed6ca11a12f1
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-06-03 20:46:30 +0700

    List Index and Last Index

---
+++ data/42 List Index and Last Index.cs
@@ -0,0 +1,18 @@
+using System; //library
+using System.Collections.Generic; //new library for list
+
+namespace TutorialCSharp //namespace
+{
+    class Program //class
+    {
+        static void Main(string[] args) //method special, automatic run program on this Main method and call a statement
+        {
+            //List
+            List<int> numbers = new List<int>() { 5, 6, 10, 42, 10, 23, 25 }; //declaration and initilization list 
+            int index = numbers.IndexOf(10); //fisrt find -> output : 2
+            int lastIndex = numbers.LastIndexOf(10); //last find -> output : 4
+            Console.WriteLine(index);
+            Console.WriteLine(lastIndex);
+        }
+    }
+}
