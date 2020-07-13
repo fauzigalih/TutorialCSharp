298b38e59e457c56f9e5c212cdd657c7ea14516d
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-07-13 19:55:51 +0700

    Linq Max Min

---
+++ data/120 Linq Max Min.cs
@@ -0,0 +1,42 @@
+using System; //library
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TutorialCSharp //namespace
+{
+    class Program //class
+    {
+        static void Main(string[] args) //method special, automatic run program on this Main method and call a statement
+        {
+            QueryList();
+        }
+
+        static void QueryList()
+        {
+            IList<int> numbers = new List<int>() { 2, 3, 5, 7 };
+
+            List<Person> persons = new List<Person>()
+            {
+                new Person(){PersonID = 1, Name = "Fauzi Galih", Age = 23},
+                new Person(){PersonID = 2, Name = "Galih", Age = 24},
+                new Person(){PersonID = 3, Name = "Pitak B Otak", Age = 29},
+                new Person(){PersonID = 4, Name = "Keleb", Age = 26}
+            };
+
+            //Max
+            int numberMax = numbers.Max();
+            int personMax = persons.Max(s => s.Age);
+
+            //Min
+            int numberMin = numbers.Min();
+            int personMin = persons.Min(s => s.Age);
+        }
+    }
+
+    class Person
+    {
+        public int PersonID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
